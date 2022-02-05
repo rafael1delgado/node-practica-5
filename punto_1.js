5ec4bbcd73830277549c645a88ff535023b6aff4
rafael1delgado <delgadorafael2011@gmail.com>
2022-02-04 21:52:55 -0400

    [update] Actualiza el punto_1.js

--- punto_1.js
+++ punto_1.js
@@ -4,13 +4,17 @@ dentro de su prototipo, las propiedades son: edad: 300, pais:"narnia"
 */
 
 let persona = {
-    nombre: "foo"
+    nombre: "foo",
+    // __proto__: {
+    //     edad: 30,
+    //     pais: "narnia"
+    // }
 };
 
-persona.__proto__.edad = 30;
+persona.__proto__.edad = 300;
 persona.__proto__.pais = "narnia";
 
-console.dir(persona);
+console.log(persona);
 console.log(persona.__proto__);
 console.log("edad: ", persona.__proto__.edad);
 console.log("pais", persona.__proto__.pais);
